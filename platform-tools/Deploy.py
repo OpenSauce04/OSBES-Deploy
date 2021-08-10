95cf01414a367ef397f958d4b0e193e99371ed9c
OpenSauce <opensauce04@gmail.com>
2021-08-10 14:53:38 +0100

    Added device detection

--- platform-tools/Deploy.py
+++ platform-tools/Deploy.py
@@ -1,6 +1,14 @@
+print("One second...")
 import os
 import subprocess
 os.system("adb kill-server > nul")
+os.system("adb start-server > nul")
+os.system("cls")
 devices=subprocess.getoutput("adb devices")
 print(devices)
+print("---------------------------------------")
+if "device\n" in devices:
+  print("Device found!")
+else: 
+  print("Device not found, please check that ADB is set up correctly on your device")
 input()
